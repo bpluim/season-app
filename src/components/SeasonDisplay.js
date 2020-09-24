eea516b588371187b24e959475c85b917c2a3d94
Ben Pluim <ben.pluim@fmgsuite.com>
2020-09-24 16:46:49 -0600

    adding seasons

--- src/components/SeasonDisplay.js
+++ src/components/SeasonDisplay.js
@@ -1,32 +1,50 @@
-import '../css/SeasonDisplay.css'
-import React from 'react'
+import '../css/SeasonDisplay.css';
+import React from 'react';
 
 const seasonConfig = {
-    summer: { textDisplay: 'Let\'s hit the beach!', iconName: 'sun' },
-    winter: { textDisplay: 'Brr, it\'s cold!', iconName: 'snowflake' }
-}
+
+  spring: {
+    text: 'April showers bring May flowers',
+    iconName: 'blind'
+  },
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: 'sun'
+  },
+  fall: {
+    text: 'Time for Pumpkin Pie',
+    iconName: 'leaf'
+  },
+  winter: {
+    text: 'Burr it is cold!',
+    iconName: 'snowflake'
+  }
+};
 
 const getSeason = (lat, month) => {
-    if (month > 2 && month < 9) {
-        return lat > 0 ? 'summer' : 'winter';
-    } else {
-        return lat > 0 ? 'winter' : 'summer';
-    }
-}
+    console.log(month);
+  if (month >= 3 && month < 6) {
+    return lat > 0 ? 'spring' : 'fall';
+  } else if (month >= 6 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else if  (month >= 9 && month < 12) {
+      return lat > 0 ? 'fall' : 'spring';
+  } else if (month === 12 || month < 3) {
+      return lat > 0 ? 'winter' : 'summer'
+  }
+};
 
-const SeasonDisplay = (props) => {
+const SeasonDisplay = props => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
 
-    const season = getSeason(props.lat, new Date().getMonth());
-    const { textDisplay, iconName } = seasonConfig[season];
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`icon-right massive ${iconName} icon`} />
+    </div>
+  );
+};
 
-    return (
-        <div className={`${season} season-display`}>
-            <i className={`${iconName} icon massive icon-left`} />
-            <h1>{ textDisplay }</h1>
-            <i className={`${iconName} icon massive icon-right`} />
-        </div>
-    )
-
-}
-
-export default SeasonDisplay
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -4,37 +4,29 @@ import SeasonDisplay from './components/SeasonDisplay';
 import Spinner from './components/Spinner'
 
 class App extends Component {
-
-    state = { 
-        lat: null, 
-        errorMessage: ''
-    };
+    state = { lat: null, errorMessage: '' };
 
     componentDidMount() {
-        window.navigator.geolocation.getCurrentPosition(
-            position => this.setState({ lat: position.coords.latitude }),
-            err => this.setState({ errorMessage: err.message}) 
-        );
+      window.navigator.geolocation.getCurrentPosition(
+        position => this.setState({ lat: position.coords.latitude }),
+        err => this.setState({ errorMessage: err.message })
+      );
     }
-
+  
     renderContent() {
-        if (this.state.errorMessage && !this.state.lat) {
-            return <div>Error: {this.state.errorMessage}</div>;
-        }
-
-        if (!this.state.errorMessage && this.state.lat) {
-            return <SeasonDisplay lat={this.state.lat}/>;
-        }
-
-        return <Spinner message="Please accept location request"/>;
+      if (this.state.errorMessage && !this.state.lat) {
+        return <div>Error: {this.state.errorMessage}</div>;
+      }
+  
+      if (!this.state.errorMessage && this.state.lat) {
+        return <SeasonDisplay lat={this.state.lat} />;
+      }
+  
+      return <Spinner message="Please accept location request" />;
     }
-
+  
     render() {
-        return (
-            <div className="border red">
-                {this.renderContent()}
-            </div>
-        )
+      return <div className="border red">{this.renderContent()}</div>;
     }
 }
 
